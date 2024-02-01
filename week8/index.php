7a6960b7d078c1bc1c9e8e4928dee320fa049971
Daniel Ramolete <ddramolete@student.apc.edu.ph>
2024-02-01 14:04:27 +0800

    PHP File Upload

--- week8/index.php
+++ week8/index.php
@@ -332,6 +332,7 @@
 </form>
 
 <?php
+
  echo "<p>Server Time: " . date("Y-m-d H:i:s") . "</p>";
  
     if ($_SERVER["REQUEST_METHOD"] == "POST") {        
@@ -350,6 +351,53 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 
+<h2>File Upload Example</h2>
+
+<?php
+// Check if form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["file"])) {
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["file"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+    // Check if file already exists
+    if (file_exists($target_file)) {
+        echo "Sorry, file already exists.";
+        $uploadOk = 0;
+    }
+
+    // Check file size
+    if ($_FILES["file"]["size"] > 500000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+
+    // Allow certain file formats
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+    }
+
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+    } else {
+        if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
+            echo "The file ". htmlspecialchars( basename( $_FILES["file"]["name"])). " has been uploaded.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
+}
+?>
+
+<form method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="file" id="file">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
 </body>
 
 </html>
