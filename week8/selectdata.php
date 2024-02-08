9a76dc4c4e4dd07a214b9057dace649b2cdedb38
Daniel D. Ramolete <143484658+dadan041100@users.noreply.github.com>
2024-02-08 16:29:08 +0800

    Update selectdata.php

--- week8/selectdata.php
+++ week8/selectdata.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "=";
-$dbname = "myDB";
+$username = "webprogss221";
+$password = "=latHen97";
+$dbname = "webprogss221";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
